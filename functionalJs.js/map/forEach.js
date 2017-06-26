7a57cf693a15258587c8c0d5036caed2c504a39c
Laura Vingada <laura@vingada.com>
2017-06-26 12:45:36 -0400

    added to functionalJs files

--- functionalJs.js/map/forEach.js
+++ functionalJs.js/map/forEach.js
@@ -47,4 +47,24 @@ function() {
 
 	return videoAndTitlePairs;
 }
+
+//To make projections easier, let's add a map() function to the Array type. Map accepts the projection function to be applied to each item in the source array, and returns the projected array.
+
+Array.prototype.map = function(projectionFunction) {
+	var results = [];
+	this.forEach(function(itemInArray) {
+
+		// ------------ INSERT CODE HERE! ----------------------------
+		// Apply the projectionFunction to each item in the array and add
+		// each result to the results array.
+		// Note: you can add items to an array with the push() method.
+		// ------------ INSERT CODE HERE! ----------------------------
+    results.push(projectionFunction(itemInArray));
+
+	});
+
+	return results;
+};
+
+// JSON.stringify([1,2,3].map(function(x) { return x + 1; })) === '[2,3,4]'
 		
