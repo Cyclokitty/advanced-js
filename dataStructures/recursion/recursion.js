8ebe4ecd4f3a3cfebf6760638801267c91b0d2f3
Laura Vingada <laura@vingada.com>
2017-06-27 18:39:37 -0400

    more recursion

--- dataStructures/recursion/recursion.js
+++ dataStructures/recursion/recursion.js
@@ -8,3 +8,36 @@ function factorial(num) {
 }
 
 // note: this is not the best way to perform a factorial function. If you make num 0 it makes a mess out of the entire thing
+
+// recursion
+// when a function calls itself until it doesn't
+
+let countDownFrom = (num) => {
+  if (num === 0) return;
+    console.log(num);
+    countDownFrom(num - 1);
+};
+
+countDownFrom(10);
+
+let categories = [
+  { id: 'animals', 'parent': null },
+  { id: 'mammals', 'parent': 'animals' },
+  { id: 'cats', 'parent': 'mammals' },
+  { id: 'dogs', 'parent': 'mammals' },
+  { id: 'chihuahua', 'parent': 'dogs' },
+  { id: 'labrador', 'parent': 'dogs' },
+  { id: 'persian', 'parent': 'cats' },
+  { id: 'siamese', 'parent': 'cats' },
+];
+
+let makeTree = (categories, parent) => {
+  let node = {};
+  categories
+    .filter(c => c.parent === parent )
+    .forEach( c => node[c.id] =
+      makeTree(categories, c.id) );
+    return node;
+};
+
+console.log(JSON.stringify(makeTree(categories, null), null, 2));
